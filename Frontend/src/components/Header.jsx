a2ce0e049f8f97a7f6c2be926243d8c4f10164b6
Kiran Dikshit <kirandikshit124@gmail.com>
2026-06-04 10:43:15 +0530

    updated

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header(){
     return <>
         <h1 className="text-5xl font-extrabold text-center mb-5 text-white">
-          Task Manager
+          TASKORA the Task Manager
         </h1>
         <p className="text-center text-slate-50 font-bold mb-10">
           Manage your daily tasks efficiently
